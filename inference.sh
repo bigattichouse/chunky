b757d8db5af14ff5701dc229cd3c6c733e295aaf
Michael Johnson <father@bigattichouse.com>
2025-06-15 23:13:16 -0500

    missing end of line

--- inference.sh
+++ inference.sh
@@ -2,7 +2,7 @@ clear
 #python simple_chunked_inference.py   --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B   --vram 4.0  --chunk-layers 2   --prompt "Explain artificial intelligence in detail"   --tokens 100   --quiet
 #Qwen/Qwen3-32B
 python chunky.py \
-  --model Qwen/Qwen3-32B
+  --model Qwen/Qwen3-32B \
   --vram 5 \
   --chunk-layers 2 \
   --prompt "Write a linux commandline tetris program in C." \
